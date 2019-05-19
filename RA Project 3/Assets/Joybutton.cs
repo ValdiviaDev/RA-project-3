ee0512453ba830595c6a45016e3a996180fca35c
Julià Mauri Costa <julimacowork@gmail.com>
2019-05-19 05:14:55 +0200

    Added joystick

---
+++ RA Project 3/Assets/Joybutton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joybutton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    [HideInInspector]
+    protected bool Pressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RA Project 3/Assets/MoveObjectFromJoystick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjectFromJoystick : MonoBehaviour
+{
+
+    protected Joystick joystick;
+    protected Joybutton joybutton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        joybutton = FindObjectOfType<Joybutton>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.velocity = new Vector3(joystick.Horizontal * 0.01f + Input.GetAxis("Horizontal") * 0.01f,
+            rigidbody.velocity.y,
+            joystick.Vertical * 0.01f + Input.GetAxis("Vertical") * 0.01f);
+    }
+}
