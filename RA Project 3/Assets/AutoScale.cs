440a132013e44cf35c593c6b67ca6edc62a338ee
Julià Mauri Costa <julimacowork@gmail.com>
2019-05-21 00:11:26 +0200

    Auto scale script

---
+++ RA Project 3/Assets/AutoScale.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoScale : MonoBehaviour
+{
+
+    public GameObject point1 = null;
+    public GameObject point2 = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (point1 && point2 && gameObject.activeInHierarchy)
+        {
+            Vector3 startPos = point1.transform.position;
+            Vector3 endPos = point2.transform.position;
+
+            Vector3 centerPos = new Vector3(startPos.x + endPos.x, startPos.y + endPos.y) / 2;
+
+            float scaleX = Mathf.Abs(startPos.x - endPos.x);
+            float scaleY = Mathf.Abs(startPos.y - endPos.y);
+
+            centerPos.x -= 0.5f;
+            centerPos.y += 0.5f;
+            gameObject.transform.position = centerPos;
+            //gameObject.transform.localScale = new Vector3(scaleX, scaleY, 1);
+        }
+    }
+}
