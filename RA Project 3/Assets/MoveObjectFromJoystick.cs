a1070a99f3a89ea24bef0f8d48d0738df1ca9105
David Valdivia <d.valdiviamartinez@gmail.com>
2019-05-20 23:22:33 +0200

    The object goes back to the entry when touches a collider

--- RA Project 3/Assets/MoveObjectFromJoystick.cs
+++ RA Project 3/Assets/MoveObjectFromJoystick.cs
@@ -9,12 +9,14 @@ public class MoveObjectFromJoystick : MonoBehaviour
     protected Joybutton joybutton;
 
     public float velocity = 0.05f;
+    private Vector3 initial_pos = new Vector3();
 
     // Start is called before the first frame update
     void Start()
     {
         joystick = FindObjectOfType<Joystick>();
         joybutton = FindObjectOfType<Joybutton>();
+        initial_pos = transform.position;
     }
 
     // Update is called once per frame
@@ -24,5 +26,15 @@ public class MoveObjectFromJoystick : MonoBehaviour
 
         rigidbody.velocity = new Vector3(joystick.Vertical * velocity + Input.GetAxis("Vertical") * velocity,
          rigidbody.velocity.y, -joystick.Horizontal * velocity + Input.GetAxis("Horizontal") * velocity);
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        Debug.Log("COLLISION XD");
+        transform.position = initial_pos;
+
+
     }
 }
