c5db71a4d0be07e763dd7048e37fe18e1760d086
Julià Mauri Costa <julimacowork@gmail.com>
2019-05-20 22:31:02 +0200

    Distance Script

---
+++ RA Project 3/Assets/DistanceImageTargets.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceImageTargets : MonoBehaviour
+{
+    public GameObject point1 = null;
+    public GameObject point2 = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(point1 && point2)
+        {
+            GetComponent<Text>().text = (point1.transform.position - point2.transform.position).ToString();
+        }
+    }
+}
