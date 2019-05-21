52b7e740beaf090b922633d118e2813d39d43dc9
David Valdivia <d.valdiviamartinez@gmail.com>
2019-05-21 20:02:45 +0200

    Start Scene

---
+++ RA Project 3/Assets/Scripts/Screen Changers/SceneChangerMain.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SceneChangerMain : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    [HideInInspector]
+    protected bool Pressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+        SceneManager.LoadSceneAsync("SampleScene");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
