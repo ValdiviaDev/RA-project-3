2b6b5c4f6d80252283df829451960c3dfb3bf0b8
David Valdivia <d.valdiviamartinez@gmail.com>
2019-05-21 17:31:35 +0200

    Sheer Heart Attack returns to original position when target is detected

---
+++ RA Project 3/Assets/Scripts/TrackingCar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class TrackingCar : DefaultTrackableEventHandler
+{
+    public GameObject SheerHA;
+
+
+    protected override void OnTrackingFound()
+    {
+        SheerHA.transform.position = Vector3.zero;
+    }
+
+    protected override void OnTrackingLost()
+    {
+        SheerHA.transform.position = Vector3.zero;
+    }
+}
+
+
