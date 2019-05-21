2392196e0b7b0ce7f47f4b3d4e5cbfc4c31a5e70
David Valdivia <d.valdiviamartinez@gmail.com>
2019-05-21 17:54:41 +0200

    Wheen Sheer Hear Attack is shown he says look over here

--- RA Project 3/Assets/Scripts/TrackingCar.cs
+++ RA Project 3/Assets/Scripts/TrackingCar.cs
@@ -6,10 +6,39 @@ public class TrackingCar : DefaultTrackableEventHandler
 {
     public GameObject SheerHA;
 
+    AudioSource source;
+    public AudioClip LookOverHere;
+
+    //Timer for sound
+    bool canPlaySound = true;
+    float soundTimer = 0.0f;
+    public float soundTime = 5.0f;
+
+    void Update()
+    {
+        //For playing Look Over Here sound
+        if (!canPlaySound)
+        {
+            soundTimer += Time.deltaTime;
+            if(soundTimer >= soundTime)
+            {
+                canPlaySound = true;
+                soundTimer = 0.0f;
+            }
+        }
+    }
 
     protected override void OnTrackingFound()
     {
+        if (canPlaySound)
+        {
+            source = SheerHA.GetComponent<AudioSource>();
+            source.PlayOneShot(LookOverHere);
+        }
+
         SheerHA.transform.position = Vector3.zero;
+        canPlaySound = false;
+        
     }
 
     protected override void OnTrackingLost()
@@ -17,5 +46,3 @@ public class TrackingCar : DefaultTrackableEventHandler
         SheerHA.transform.position = Vector3.zero;
     }
 }
-
-
