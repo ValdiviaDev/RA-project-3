27bf022bc32cfa6989d231ef9177d0bd5363b72c
David Valdivia <d.valdiviamartinez@gmail.com>
2019-05-21 21:42:35 +0200

    Start and finish scenes done

---
+++ RA Project 3/Assets/Scripts/UI interaction/CloseGame.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CloseGame : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    [HideInInspector]
+    protected bool Pressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
